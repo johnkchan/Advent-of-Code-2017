5526daf506418aa831a11831fd360264b4326031
Jonathan Chan <johnkchan@users.noreply.github.com>
2017-12-06 09:56:34 -0500

    Update 6.1.py

--- Day06/6.1.py
+++ Day06/6.1.py
@@ -2,9 +2,7 @@ key = "4	1	15	12	0	9	9	5	5	8	7	3	14	5	12	3"
 key = [int(i) for i in key.split("\t")]
 
 container = []
-counter = 0
 conditionMet = False
-
 while not conditionMet:
     maxValue = max(key)
     maxIndex = key.index(maxValue)
@@ -13,10 +11,8 @@ while not conditionMet:
     for i in range(maxIndex + 1, maxIndex + maxValue + 1):
         key[i % len(key)] += 1
 
-    counter += 1
-
     if key not in container:
         container.append([i for i in key])
     else:
         conditionMet = True
-        print(counter)
+        print(len(container) + 1)
